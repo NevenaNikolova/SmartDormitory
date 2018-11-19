e5bfcf90203191f4202c4d0fd0768cd13649c469
NevenaN <n_milanova@mail.bg>
2018-11-19 17:00:33 +0200

    initial commit

---
+++ DormitorySystem/DormitorySystem.Data/Models/Sensor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DormitorySystem.Data.Models
+{
+    public class Sensor
+    {
+        public Guid Id { get; set; }
+
+
+        public int TypeId { get; set; }
+        public Type Type { get; set; }
+        public string Description { get; set; }
+        public int PollingInterval { get; set; }
+        public int Value { get; set; }
+
+    }
+}
---
+++ DormitorySystem/DormitorySystem.Data/Models/Type.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DormitorySystem.Data.Models
+{
+    public class Type
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Sensor> Sensors { get; set; }
+    }
+}
