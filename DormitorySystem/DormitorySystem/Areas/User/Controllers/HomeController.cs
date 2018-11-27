ca832c9edbed372ea7ce5467af405d540f36a744
Antonio Iliev <iliev.t.a@gmail.com>
2018-11-27 14:15:41 +0200

    Register new sensor

--- DormitorySystem/DormitorySystem/Areas/User/Controllers/HomeController.cs
+++ DormitorySystem/DormitorySystem/Areas/User/Controllers/HomeController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DormitorySystem.Data.Models;
+using DormitorySystem.Services.Abstractions;
+using DormitorySystem.Web.Areas.User.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,10 +13,30 @@ namespace DormitorySystem.Web.Areas.User.Controllers
     [Area("User")]
     [Authorize(Roles = "User, Admin")]
     public class HomeController : Controller
-    {      
+    {
+        private readonly IUserSensorService sensorService;
+
+        public HomeController(IUserSensorService sensorService)
+        {
+            this.sensorService = sensorService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        public IActionResult ViewSensorTypes()
+        {
+            var model = new SensorsByTypeViewModel(this.sensorService.GetSensorsByType());
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult ListSensorByType(IEnumerable<SampleSensor> sensors)
+        {
+            return View(sensors);
         }
 
         [HttpGet]
---
+++ DormitorySystem/DormitorySystem/Areas/User/Models/SensorsByTypeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DormitorySystem.Data.Models;
+using System.Collections.Generic;
+
+namespace DormitorySystem.Web.Areas.User.Models
+{
+    public class SensorsByTypeViewModel
+    {
+        public SensorsByTypeViewModel(IDictionary<string, IEnumerable<SampleSensor>> sensors)
+        {
+            this.SampleSensorsByType = sensors;
+        }
+
+        public IDictionary<string, IEnumerable<SampleSensor>> SampleSensorsByType { get; set; }
+
+    }
+}
