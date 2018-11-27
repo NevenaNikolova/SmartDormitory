51bd770febcb078072ac7c4fcb14dcf5a3abc55c
NevenaN <n_milanova@mail.bg>
2018-11-27 10:32:54 +0200

    Fixed Admin Add User To Role

--- DormitorySystem/DormitorySystem/Areas/Admin/Controllers/HomeController.cs
+++ DormitorySystem/DormitorySystem/Areas/Admin/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using DormitorySystem.Data.Models;
 using DormitorySystem.Services.Abstractions;
+using DormitorySystem.Services.Exceptions;
 using DormitorySystem.Web.Areas.Admin.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,12 +17,15 @@ namespace DormitorySystem.Web.Areas.Admin.Controllers
     {
         private readonly IUserService _userService;
         private readonly UserManager<Data.Models.User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         
         public HomeController(IUserService userService, 
-            UserManager<Data.Models.User> userManager)
+            UserManager<Data.Models.User> userManager,
+           RoleManager<IdentityRole>roleManager)
         {
             _userService = userService;
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         public IActionResult Index()
@@ -42,12 +47,53 @@ namespace DormitorySystem.Web.Areas.Admin.Controllers
             var roles= await this._userManager.GetRolesAsync(user);
             return View(new UserViewModel(user, string.Join(", ", roles)));
         }
-        public async Task<IActionResult> AssignRoles(string id)
+        public async Task<IActionResult> Roles(string id)
         {
-            var user = this._userService.GetUser(id);
-            var result = await this._userManager.AddToRoleAsync(user, "Admin");
-            return View();
+            var user = await this._userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                throw new UserNullableException("There is no such user.");
+            }
+            var roles = await this._userManager.GetRolesAsync(user);
+            return View(new UserWithRolesViewModel
+            {
+                Id=user.Id,
+                UserName=user.UserName,
+                Email=user.Email,
+                Roles=roles
+            });
         }
+        public IActionResult AddToRole(string id)
+        {
+            var rolesSelectListItems = this._roleManager
+                .Roles
+                .Select(r => new SelectListItem
+                {
+                    Text = r.Name,
+                    Value = r.Name
+                })
+                .ToList();
+            return View(rolesSelectListItems);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddToRole(string id, string role)
+        {
+            var user = await this._userManager.FindByIdAsync(id);
+            var roleExists = await this._roleManager.RoleExistsAsync(role);
+
+            if (user == null||!roleExists)
+            {
+                return NotFound();
+            }
+            await this._userManager.AddToRoleAsync(user, role);
+          
+            TempData["SuccessMessage"] = $"User {user.Email} added to {role} role!";
+
+            return RedirectToAction(nameof(Index));
+        }
+
         public IActionResult Sensors(string id)
         {
             return View();
