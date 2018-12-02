0df78f1d53582d75c3c2e396ed9b99b4c920fe18
Antonio Iliev <iliev.t.a@gmail.com>
2018-12-02 12:42:15 +0200

    Add setInterval

--- DormitorySystem/DormitorySystem/wwwroot/js/test.js
+++ DormitorySystem/DormitorySystem/wwwroot/js/test.js
@@ -1,55 +1,54 @@
 ﻿let url = '/api/test';
 
+setInterval(update, 5000)
 
-$.get(url, function (data) {
+function update() {
 
-    var el = document.getElementById('graph'); // get canvas
+    $.get(url, function (data) {
 
-    var options = {
-        percent: data,
-        size: el.getAttribute('data-size') || 220,
-        lineWidth: el.getAttribute('data-line') || 15,
-        rotate: el.getAttribute('data-rotate') || 0
-    }
+        var el = document.getElementById('graph'); // get canvas
 
-    var canvas = document.createElement('canvas');
-    var span = document.createElement('span');
-    span.textContent = options.percent + '%';
+        var options = {
+            percent: data,
+            size: el.getAttribute('data-size') || 220,
+            lineWidth: el.getAttribute('data-line') || 15,
+            rotate: el.getAttribute('data-rotate') || 0
+        }
 
-    if (typeof (G_vmlCanvasManager) !== 'undefined') {
-        G_vmlCanvasManager.initElement(canvas);
-    }
+        var canvas = document.createElement('canvas');
+        var span = document.createElement('span');
+        span.textContent = options.percent + '%';
 
-    var ctx = canvas.getContext('2d');
-    canvas.width = canvas.height = options.size;
+        if (typeof (G_vmlCanvasManager) !== 'undefined') {
+            G_vmlCanvasManager.initElement(canvas);
+        }
 
-    el.appendChild(span);
-    el.appendChild(canvas);
+        var ctx = canvas.getContext('2d');
+        canvas.width = canvas.height = options.size;
 
-    ctx.translate(options.size / 2, options.size / 2); // change center
-    ctx.rotate((-1 / 2 + options.rotate / 180) * Math.PI); // rotate -90 deg
+        el.appendChild(span);
+        el.appendChild(canvas);
 
-    //imd = ctx.getImageData(0, 0, 240, 240);
-    var radius = (options.size - options.lineWidth) / 2;
+        ctx.translate(options.size / 2, options.size / 2); // change center
+        ctx.rotate((-1 / 2 + options.rotate / 180) * Math.PI); // rotate -90 deg
 
-    var drawCircle = function (color, lineWidth, percent) {
-        percent = Math.min(Math.max(0, percent || 1), 1);
-        ctx.beginPath();
-        ctx.arc(0, 0, radius, 0, Math.PI * 2 * percent, false);
-        ctx.strokeStyle = color;
-        ctx.lineCap = 'round'; // butt, round or square
-        ctx.lineWidth = lineWidth
-        ctx.stroke();
-    };
+        //imd = ctx.getImageData(0, 0, 240, 240);
+        var radius = (options.size - options.lineWidth) / 2;
 
-    drawCircle('#efefef', options.lineWidth, 100 / 100);
-    drawCircle('#555555', options.lineWidth, options.percent / 100);
+        var drawCircle = function (color, lineWidth, percent) {
+            percent = Math.min(Math.max(0, percent || 1), 1);
+            ctx.beginPath();
+            ctx.arc(0, 0, radius, 0, Math.PI * 2 * percent, false);
+            ctx.strokeStyle = color;
+            ctx.lineCap = 'round'; // butt, round or square
+            ctx.lineWidth = lineWidth
+            ctx.stroke();
+        };
 
-}).fail(function () {
-    alert("Ops! Something happened");
-});
+        drawCircle('#efefef', options.lineWidth, 100 / 100);
+        drawCircle('#555555', options.lineWidth, options.percent / 100);
 
-function autoRefreshPage() {
-    window.location = window.location.href;
+    }).fail(function () {
+        alert("Ops! Something happened");
+    });
 }
-setInterval('autoRefreshPage()', 50000);
