7345e47f4428bdb768595a174dd01a8925898588
NevenaN <n_milanova@mail.bg>
2018-12-17 09:22:01 +0200

    delete modal finally works

--- DormitorySystem/DormitorySystem/wwwroot/js/delete.js
+++ DormitorySystem/DormitorySystem/wwwroot/js/delete.js
@@ -3,25 +3,6 @@
     var redirectUrl;
     var target;
 
-    $('body').append(`
-                    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <h4 class="modal-title" id="myModalLabel">Warning</h4>
-                        </div>
-                        <div class="modal-body delete-modal-body">                          
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal" id="cancel-delete">Cancel</button>
-                            <button type="button" class="btn btn-danger" id="confirm-delete">Delete</button>
-                        </div>
-                        </div>
-                    </div>
-                    </div>`);
-
-    //Delete Action
     $(".delete").on('click', (e) => {
         e.preventDefault();
 
